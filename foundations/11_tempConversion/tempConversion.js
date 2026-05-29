cc36d570fe5c4ac044fb4225e6fc1cca7aa95ab6
LuminousFondue <4483859+LuminousFondue@users.noreply.github.com>
2026-05-29 15:13:01 +0000

    Add solution for foundations/11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  let temp = (fTemp - 32) * (5 / 9);
+  return parseFloat(temp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  let temp = cTemp * (9 / 5) + 32
+  return parseFloat(temp.toFixed(1))
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
