790b4476d9ddcce594e0c5d1df8f30bd4402e5cf
LuminousFondue <4483859+LuminousFondue@users.noreply.github.com>
2026-05-29 14:00:26 +0000

    Add tests for foundations/06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function(str, x) {
+const repeatString = function (str, x) {
+    if (x < 0) { return "ERROR"; }
     let output = "";
-    for(i = 0; i < x; i++){
+    for (i = 0; i < x; i++) {
         output = output.concat(str);
     }
     return output;
