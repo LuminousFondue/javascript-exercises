d573972c63dc740aee057388b3770a2d8acedf98
LuminousFondue <4483859+LuminousFondue@users.noreply.github.com>
2026-05-29 13:24:45 +0000

    Add solution for foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+    if (int1 < 0 ||
+        int2 < 0 ||
+        Number.isInteger(int1) == false ||
+        Number.isInteger(int2) == false) {
+        return "ERROR"
+    }
 
+    const min = Math.min(int1, int2);
+    const max = Math.max(int1, int2);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
