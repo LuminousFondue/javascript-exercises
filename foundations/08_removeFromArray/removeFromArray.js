1c5f40f44c273e90ea581759011343010c2f4e22
LuminousFondue <4483859+LuminousFondue@users.noreply.github.com>
2026-05-29 14:48:48 +0000

    Add tests for foundations/08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function(arr, numToRemove) {
-    return arr.filter((val)=> val != numToRemove);
+const removeFromArray = function (arr, argsIn) {
+    let args = Array.from(arguments);
+    for (let i = 1; i < args.length; i++) {
+        let valToRemove = args[i];
+        arr = arr.filter((val) => (val != valToRemove) || (typeof valToRemove != typeof val));
+    }
+    return arr
 };
+
+let array = [1, 2, 3, 4]
+console.log(array);
+
+console.log(removeFromArray(array, "1", 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
