a308714b356621cf9ba505324829ea525a0f144b
LuminousFondue <4483859+LuminousFondue@users.noreply.github.com>
2026-05-29 12:29:45 +0000

    Add solution for foundations/06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, x) {
+    let output = "";
+    for(i = 0; i < x; i++){
+        output = output.concat(str);
+    }
+    return output;
 };
 
 // Do not edit below this line
